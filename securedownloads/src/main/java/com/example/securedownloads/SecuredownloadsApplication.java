f882dd77a23ae0ba54a5d76e7236e0bd080aa4a4
Kaushik Jain <107410207+jain2001@users.noreply.github.com>
2025-06-07 00:40:46 +0530

    Add files via upload

---
+++ securedownloads/src/main/java/com/example/securedownloads/SecuredownloadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.securedownloads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredownloadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredownloadsApplication.class, args);
+	}
+
+}
---
+++ securedownloads/src/test/java/com/example/securedownloads/SecuredownloadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securedownloads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredownloadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
